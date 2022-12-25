579582aba241aeb6c6e348599200a14a0d503723
Luda <108550003+Snowwhiteswan@users.noreply.github.com>
2022-12-25 18:46:39 +0100

    Update course3_week3_tasks.c

--- course3_week3_tasks.c
+++ course3_week3_tasks.c
@@ -1,47 +1,3 @@
-/* TASK 3
-*
-* Within this program, we will pass an array with 6 integers to a function, have the function swap the first and last integer,
-* the second and the second to last integer, the third and the third to last integer.
-* The function is called reverseArray and doesn't return anything (void). It should take one parameter, 
-* representing the array of integers. 
-* The main function first reads 6 integers from the input, and assigns them to the array. The main function then 
-* calls reverseArray, passing the array as an argument.
-* The main function then prints the reversed array.
-*/
-
-#include <stdio.h>
-void reverseArray(int*);
-
-int main(void){
-
-int arr[6];
-//I read the integers and assign them to the array
-for (int i=0;i<6;i++){
-scanf("%d", &arr[i]);
-}
-//I call the function
-reverseArray(arr);
-
-//I print the changed array
- for (int i=0;i<6;i++){
-        printf("%d ", *(arr+i));
-    }
-return 0;
-}
-void reverseArray (int * numbers){
-int temp;
-temp = * numbers;
-* numbers = * (numbers+5);
-* (numbers+5) = temp;
-temp = *(numbers+1);
-*(numbers+1) = *(numbers + 4);
-*(numbers+4) = temp;
-temp = *(numbers + 2);
-*(numbers+2)= *(numbers+3);
-*(numbers+3)=temp;
-}
-
-
 
 /* TASK 1
 
@@ -121,3 +77,47 @@ void change (int * a){
 if (*a >21){
 *a = *a -10;}
 else{*a = * a * 2;}}
+
+/* TASK 3
+*
+* Within this program, we will pass an array with 6 integers to a function, have the function swap the first and last integer,
+* the second and the second to last integer, the third and the third to last integer.
+* The function is called reverseArray and doesn't return anything (void). It should take one parameter, 
+* representing the array of integers. 
+* The main function first reads 6 integers from the input, and assigns them to the array. The main function then 
+* calls reverseArray, passing the array as an argument.
+* The main function then prints the reversed array.
+*/
+
+#include <stdio.h>
+void reverseArray(int*);
+
+int main(void){
+
+int arr[6];
+//I read the integers and assign them to the array
+for (int i=0;i<6;i++){
+scanf("%d", &arr[i]);
+}
+//I call the function
+reverseArray(arr);
+
+//I print the changed array
+ for (int i=0;i<6;i++){
+        printf("%d ", *(arr+i));
+    }
+return 0;
+}
+void reverseArray (int * numbers){
+int temp;
+temp = * numbers;
+* numbers = * (numbers+5);
+* (numbers+5) = temp;
+temp = *(numbers+1);
+*(numbers+1) = *(numbers + 4);
+*(numbers+4) = temp;
+temp = *(numbers + 2);
+*(numbers+2)= *(numbers+3);
+*(numbers+3)=temp;
+}
+
